3bbd1fd0bd063fbe0e5c214a9a9d25a89209c31d
xukaishan <44866370+xukaishan@users.noreply.github.com>
2019-11-22 09:19:20 +0800

    Update func.js

--- func.js
+++ func.js
@@ -20,3 +20,23 @@
 			}
 		}
 	}
+/* 加载更多 */
+ function scrollToBottom(number = 0, callback, once = false) {
+                const doc = document;
+                /** 滚动事件 */
+                function onScroll() {
+                    /** 滚动的高度 */
+                    let scrollTop = doc.documentElement.scrollTop === 0 ? doc.body.scrollTop : doc.documentElement.scrollTop;
+                    /** 滚动条高度 */
+                    let scrollHeight = doc.documentElement.scrollTop === 0 ? doc.body.scrollHeight : doc.documentElement.scrollHeight;
+                    if (scrollHeight - scrollTop - number <= window.innerHeight) {
+                        if (typeof callback === 'function') callback();
+                        if (once) window.removeEventListener('scroll', onScroll);
+                    }
+                }
+                window.addEventListener('scroll', onScroll);
+                onScroll(); // 先执行一次
+            }
+            scrollToBottom(100, function () {
+                console.log('到底底部~');
+            }, false);
