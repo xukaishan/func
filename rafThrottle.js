e65b5e952bd0ad923f858caaa08a46a154414796
xukaishan <44866370+xukaishan@users.noreply.github.com>
2020-11-13 11:04:14 +0800

    Create rafThrottle.js

---
+++ rafThrottle.js
@@ -0,0 +1,12 @@
+/** 动画节流 */
+function rafThrottle(fn) {
+  let locked = false;
+  return function(...args) {
+    if (locked) return;
+    locked = true;
+    window.requestAnimationFrame(_ => {
+      fn.apply(this, args);
+      locked = false;
+    });
+  };
+}
