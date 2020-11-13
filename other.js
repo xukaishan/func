f3a7042b854edcccb2f52730b66e40a6b4249e31
xukaishan <44866370+xukaishan@users.noreply.github.com>
2020-11-13 10:55:22 +0800

    Create other.js

---
+++ other.js
@@ -0,0 +1,19 @@
+const getValueByPath = function(object, prop) {
+  prop = prop || '';
+  const paths = prop.split('.');
+  let current = object;
+  let result = null;
+  for (let i = 0, j = paths.length; i < j; i++) {
+    const path = paths[i];
+    if (!current) break;
+
+    if (i === j - 1) {
+      result = current[path];
+      break;
+    }
+    current = current[path];
+  }
+  return result;
+}
+
+getValueByPath({a:{b:{c:1,d:2}}},'a.b.c') // 1
